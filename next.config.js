cc2872ca455143325647b0164e8d955c06bf78af
Lovedeep <gurugobind.sh@gmail.com>
2023-07-16 09:50:20 +0530

    url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    
+    baseUrl: 'https://bulk-messaging-app-frontend.vercel.app/',
+
+  
 }
 
 module.exports = nextConfig
