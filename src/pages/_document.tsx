fe21864f34b425685183219af59aed9055300c8d
Lovedeep <gurugobind.sh@gmail.com>
2023-07-17 18:25:40 +0530

    dark mode is much better

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-      <ColorModeScript />
+      <ColorModeScript initialColorMode="dark" />
         <Main />
         <NextScript />
       </body>
